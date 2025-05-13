b16cbc985151a7e03329e357b366162359519436
B-Rakevicius <115837262+B-Rakevicius@users.noreply.github.com>
2025-05-13 13:56:55 +0300

    Button onhover/onclick sounds
    Added two mp3's for onclick and onhover, various scene sounds withheld until scene assets are updated.

--- Assets/Scripts/MainButtonSelected.cs
+++ Assets/Scripts/MainButtonSelected.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
-
-public class MainButtonSelected : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class MainButtonSelected : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    [SerializeField] [Range(0,2)] float onHoverMultiplyer;
-
+    [Header("Visual Settings")]
+    [SerializeField][Range(0, 2)] float onHoverMultiplyer;
+    [Header("Sound Settings")]
+    [SerializeField] private string hoverSoundName = "ButtonHover";
+    [SerializeField] private string clickSoundName = "ButtonClick";
+    [Range(0, 1)] public float hoverVolume = 1f;
+    [Range(0, 1)] public float clickVolume = 1f;
     RectTransform rect;
-
     Vector3 oldSize, newSize;
-
     private void Start()
     {
         rect = GetComponent<RectTransform>();
@@ -26,21 +28,35 @@ public class MainButtonSelected : MonoBehaviour, IPointerEnterHandler, IPointerE
     public void ResetSize()
     {
         rect.DOKill();
-
         rect.localScale = oldSize;
     }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
         rect.DOKill();
-
         rect.DOScale(newSize, 0.1f).SetEase(Ease.InOutCubic);
+        PlaySound(hoverSoundName, hoverVolume);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         rect.DOKill();
-
         rect.DOScale(oldSize, 0.1f).SetEase(Ease.InOutCubic);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlaySound(clickSoundName, clickVolume);
+    }
+
+    private void PlaySound(string soundName, float volume)
+    {
+        if (!string.IsNullOrEmpty(soundName) && SoundManager.Instance != null)
+        {
+            var sound = SoundManager.Instance.GetSound(soundName);
+            if (sound != null)
+            {
+                sound.ChangeVolume(volume).PlayOneShot();
+            }
+        }
     }
 }
